77434bb9445d99206c08708c415ab13fdb6857ec
simonrichard-dev <simon.insa@gmail.com>
2024-04-16 13:49:51 +0200

    add folder for task 1. Let s build a Teacher interface

---
+++ TypeScript/task_1/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// main.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,11 +1,21 @@
 // main.ts
 
-interface Teacher {
-    firstName: string;
-    lastName: string;
+export interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
     fullTimeEmployee: boolean;
-    yearsOfExperience: number;
+    yearsOfExperience?: number;
     location: string;
+    [propName: string]: any;
   }
 
-  
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
